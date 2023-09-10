2e4768c8e80e1b5f10c6b9a5bd65aca9a76c570c
duylue <binhminhbenem2801@gmail.com>
2023-09-10 12:11:14 +0700

    asss

--- src/main/java/com/productk12Api/model/Role.java
+++ src/main/java/com/productk12Api/model/Role.java
@@ -1,12 +1,23 @@
 package com.productk12Api.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 @Getter
 @Setter
+@Table(name="roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int rid;
+    @Column(nullable = false)
     private String name;
-
+    @JsonIgnore
+    @ManyToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER,mappedBy = "roles")
+    private List<User> users;
 }
--- src/main/java/com/productk12Api/model/User.java
+++ src/main/java/com/productk12Api/model/User.java
@@ -5,18 +5,23 @@ import lombok.*;
 import javax.persistence.*;
 import java.util.List;
 
+@Entity
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int uid;
     private String name;
-
+    @Column(length = 30, unique = true, nullable = false)
     private String username;
-
+    @Column(nullable = false)
     private String password;
-
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role",
+            joinColumns = {@JoinColumn(name = "uid", referencedColumnName = "uid")},
+            inverseJoinColumns = {@JoinColumn(name = "rid", referencedColumnName = "rid")})
     private List<Role> roles;
 }
+
